ad782c2d617890a327cdc8776a3490d9f6f14d18
Thorge Neelsen <thorge@roamsys.com>
2018-03-20 11:21:24 +0100

    Added function(shadeColor) to StatusBar backgroundColor and StatusBar background is animated

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ import DoneButton from './buttons/DoneButton';
 // hotfix: https://github.com/facebook/react-native/issues/16710
 const itemVisibleHotfix = { itemVisiblePercentThreshold: 100 };
 
+function shadeColor(color, percent) {
+	var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+	return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
+
 class Onboarding extends Component {
 	constructor() {
 		super();
@@ -31,7 +36,7 @@ class Onboarding extends Component {
 	componentDidUpdate() {
 		Animated.timing(this.state.backgroundColorAnim, {
 			toValue: 1,
-			duration: 500,
+			duration: 300,
 		}).start();
 	}
 
@@ -122,7 +127,7 @@ class Onboarding extends Component {
 		return (
 			<SafeAreaView
 		style={{ flex: 1, backgroundColor, justifyContent: 'center' }}>
-	<StatusBar barStyle={this.state.gone ? 'default' : barStyle} />
+	<StatusBar barStyle={this.state.gone ? 'default' : barStyle} backgroundColor={shadeColor(currentBackgroundColor, -0.20)} animated/>
 		<FlatList
 		ref={list => {
 			this.flatList = list;
